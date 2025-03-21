7c8b60616b6e004b0af09906fab723ab7d316eac
AjinkyaBhosale1998 <bhosaleajinkya1998@gmail.com>
2025-03-21 23:30:49 +0530

    added product and cart setup design

---
+++ src/Cart.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Cart.css'
+
+const Cart = () => {
+  return (
+    <>
+        <h4>Cart Summary</h4>
+        <div>
+            <button className='incraese'>+</button>
+            <button className='decrese'>-</button>
+        </div>
+        
+    </>
+
+  )
+}
+
+export default Cart
