e99ba4e34708f6c6d8daebf4e4e0685906e35fd8
AjinkyaBhosale1998 <bhosaleajinkya1998@gmail.com>
2025-03-22 16:37:59 +0530

    added cart summary, subtotal , total and gift logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,35 @@ import './App.css'
 import Cart from './Cart';
 
 function App() {
+  const [cart, setCart] = useState([]);
+  
+  const addtocart = (product) => {
+    setCart((prevCart) => {
+      const itemsExists = prevCart.find((item) => item.id === product.id);
+      if (itemsExists) {
+        return prevCart.map((item) => item.id=== product.id ? {...item, quantity: item.quantity + 1} : item);
+      
+    }else{
+      return [...prevCart, {...product, quantity:1}];
+    }})
+  }
 
+  const updateQantity  = (id, amount) => {
+    setCart((prevCart) => prevCart.map((item)=> item.id === id ? { ...item, quantity:Math.max(1, item.quantity + amount)} : item)
+    .filter((item) => item.quantity > 0) 
+  );
+  }
+
+   const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+   const freeGiftApplied = total >= 1000;
   const PRODUCTS = [
     { id: 1, name: "Laptop", price: 500 },
     { id: 2, name: "Smartphone", price: 300 },
     { id: 3, name: "Headphones", price: 100 },
     { id: 4, name: "Smartwatch", price: 150 },
   ];
-  const addtocart = (product) => {
-    
-    console.log("hi addtocart has been clicked ");
-  };
+  
 
   return (
     <>
@@ -31,7 +49,7 @@ function App() {
       ))}
       
      </div>
-     <Cart/>
+     <Cart cart={cart} updateQantity={updateQantity} total={total} freeGiftApplied={freeGiftApplied}/>
     </>
   )
 }
--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import './Cart.css'
 
-const Cart = () => {
-  return (
-    <>
-        <h4>Cart Summary</h4>
-        <div>
-            <button className='incraese'>+</button>
-            <button className='decrese'>-</button>
+const Cart = ({cart, updateQantity, total, freeGiftApplied}) => {
+    return (
+        <div className='cart'>
+            <h4>Cart Summary</h4>
+            {cart.length === 0 ? (
+                <p>Your cart is empty</p>
+            ) : (
+                <>
+                    {cart.map((item) => (
+                        <div key={item.id} className='cart-item'>
+                            <span>{item.name} -${item.price}* {item.quantity}</span>
+                            {item.id !== 99 && (
+                                <div>
+                                    <button className='increase' onClick={() => updateQantity(item.id, 1)}>+</button>
+                                    <button className='decrease' onClick={() => updateQantity(item.id, -1)}>-</button>
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                    <h3>Subtotal: ${total}</h3>
+                    {freeGiftApplied && <p className='gift'>🎁 Free Gift Applied !</p>}
+                </>
+            )}
         </div>
-        
-    </>
 
-  )
+    )
 }
 
 export default Cart
